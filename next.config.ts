ecc351e9ab1d726b58507df434da631e86a31ff9
Brenden Moore <brendenm17@gmail.com>
2024-11-21 00:30:38 -0500

    ci fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/secret-santa-app",
+  output: "export",
   reactStrictMode: true,
 };
 
